1ca69ccab301b5368bebc976a712df6b650a1cd5
ArslanRasheed60 <arslanrashee1@gmail.com>
2023-04-16 12:34:49 +0500

    Adding signup and login xml

---
+++ app/src/main/java/com/example/chatapp/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.chatapp.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
